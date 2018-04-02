2e48ffdcf9e95976fe30709b57f1efd39b99d4f9
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2018-04-02 11:40:16 +0300

    Add arc theme to list of installation scripts

---
+++ installation_scripts/arc-theme.sh
@@ -0,0 +1,11 @@
+sudo apt-get install autoconf automake libgtk-3-dev
+
+timestamp=$(date +%s)
+arc_theme_source_dir=/tmp/arc-theme-$timestamp
+
+git clone https://github.com/horst3180/arc-theme --depth 1 $arc_theme_source_dir
+cd $arc_theme_source_dir
+
+./autogen.sh --prefix=/usr
+sudo make install
+
