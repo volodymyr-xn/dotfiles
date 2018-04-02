f1e1ceaa111613ac977231f3f0e8e667827b2b16
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2018-04-02 11:34:13 +0300

    Improve ruby-asdf installation script

--- installation_scripts/ruby_asdf.sh
+++ installation_scripts/ruby_asdf.sh
@@ -2,10 +2,10 @@ asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 
 last_ruby_version=$(asdf list-all ruby | tail -n 1)
 
-old_ruby_version=$(asdf list ruby | head -n 1)
-echo "Remove old ruby version $old_ruby_version"
-asdf uninstall ruby $old_ruby_version
-
 echo "Install ruby $last_ruby_version"
 asdf install ruby $last_ruby_version
 asdf global ruby $last_ruby_version
+
+gem install bundler
+gem install tmuxinator
+asdf reshim ruby
