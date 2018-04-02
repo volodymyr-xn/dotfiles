89cd0449889cac992cfbc1af684938983a1e9a67
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2018-04-02 23:13:57 +0300

    Add script for software install

---
+++ install_software.sh
@@ -0,0 +1,27 @@
+./installation_scripts/alacritty.sh
+./installation_scripts/arc-theme.sh
+./installation_scripts/asdf.sh
+./installation_scripts/base16_shell.sh
+./installation_scripts/crystal-asdf.sh
+./installation_scripts/ctags.sh
+./installation_scripts/develop_libs.sh
+./installation_scripts/docker.sh
+./installation_scripts/fzf.sh
+
+# install oracle java
+./installation_scripts/java.sh
+
+./installation_scripts/libevent.sh
+./installation_scripts/nodejs_asdf.sh
+./installation_scripts/numix-circle.sh
+./installation_scripts/oh_my_zsh.sh
+./installation_scripts/postgres.sh
+./installation_scripts/redis.sh
+./installation_scripts/ruby_asdf.sh
+./installation_scripts/silver_searcher.sh
+./installation_scripts/tmux.sh
+./installation_scripts/tweak_tool.sh
+./installation_scripts/vim8.sh
+./installation_scripts/vim_plug.sh
+./installation_scripts/xdotool.sh
+./installation_scripts/zsh.sh
