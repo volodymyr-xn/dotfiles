e88d4548234d82be20efcfd2cdc148bbbb8cc1c0
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-01-07 01:16:26 +0200

    Fix Alacrity installation script

--- installation_scripts/alacritty.sh
+++ installation_scripts/alacritty.sh
@@ -1,8 +1,14 @@
 echo "Installing rust"
-# # install run
+
+# Install Rustup
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
-#
+
+# Install last stable version of Rust
+rustup override set stable
+rustup update stable
+
+
 # # install dependencies
 sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
 
@@ -15,6 +21,6 @@ git clone https://github.com/jwilm/alacritty.git $alacritty_source_tmp_path
 cd $alacritty_source_tmp_path && cargo build --release
 
 sudo cp $alacritty_source_tmp_path/target/release/alacritty /usr/local/bin/alacritty
-cp Alacritty.desktop ~/.local/share/applications
+cp alacritty.desktop ~/.local/share/applications
 
 rm -rf $alacritty_source_tmp_path
