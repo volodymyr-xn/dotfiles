946a7c80ec771f90e812afd1e9ce5addfc233690
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-01-08 22:29:07 +0200

    Update Ruby version to 2.6

--- installation_scripts/ruby_asdf.sh
+++ installation_scripts/ruby_asdf.sh
@@ -1,6 +1,8 @@
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 
-last_ruby_version=$(asdf list-all ruby | tail -n 1)
+source ~/dotfiles/packages_versions.sh
+
+last_ruby_version=$ruby_version
 
 echo "Install ruby $last_ruby_version"
 asdf install ruby $last_ruby_version
