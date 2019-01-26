06f0f3742510a76ab6c8b195152c75ea15d86665
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-01-26 18:38:10 +0200

    Update xdotool installation script

--- installation_scripts/xdotool.sh
+++ installation_scripts/xdotool.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-sudo apt-get install xdotool
+# sudo apt-get install xdotool
+
+timestamp=$(date +%s)
+xdotool_source_dir=/tmp/xdotool-source-$timestamp
+
+echo "Stating installl"
+git clone https://github.com/jordansissel/xdotool $xdotool_source_dir
+
+echo "Stating compilation process"
+( cd $xdotool_source_dir && make -j 8 && sudo make install -j 8)
