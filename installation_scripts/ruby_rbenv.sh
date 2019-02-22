ab42e61840e60a0db93600f126c847e81a949763
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-02-22 09:17:59 +0200

    Improve dconf config

--- installation_scripts/ruby_rbenv.sh
+++ installation_scripts/ruby_rbenv.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-exec $SHELL
-
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-exec $SHELL
 
-rbenv install 2.4.1
-rbenv global 2.4.1
+rbenv install 2.6.1
+rbenv global 2.6.1
--- set_dconf.sh
+++ set_dconf.sh
@@ -20,3 +20,9 @@ custom_keybinding_0_schema=/org/gnome/settings-daemon/plugins/media-keys/custom-
 dconf write $custom_keybinding_0_schema/name "'Toggle display focus'"
 dconf write $custom_keybinding_0_schema/command "'toggle-display-focus'"
 dconf write $custom_keybinding_0_schema/binding "'<Control>space'"
+
+# Gnome terminal settings
+gnome_terminal=$(gsettings get org.gnome.Terminal.ProfilesList default)
+gnome_terminal=${gnome_terminal:1:-1} # remove leading and trailing single quotes
+gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$gnome_terminal/" font "Fira Mono Medium 14"
+
