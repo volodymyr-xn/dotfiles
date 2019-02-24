9bebf14c1bda0e3576c02f19e35ecc64b42ec2f6
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-02-24 07:45:56 +0200

    Add "install_executables" installation file

---
+++ install_executables.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Create dir for executables
+mkdir -p $home/bin
+
+ln -nfs $HOME/dotfiles/bin/toggle-display-focus $HOME/bin/toggle-display-focus
--- setup_system.sh
+++ setup_system.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+./install_executables.sh
 ./install_software.sh
 ./install_dotfiles.sh
 ./set_dconf.sh
