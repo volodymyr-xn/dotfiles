23450d54c8cabaf703aaf88c326a484fce4bc803
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-03-13 21:12:09 +0200

    Improve vimrc

--- install_executables.sh
+++ install_executables.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Create dir for executables
-mkdir -p $home/bin
+mkdir -p $HOME/bin
 
 ln -nfs $HOME/dotfiles/bin/toggle-display-focus $HOME/bin/toggle-display-focus
+
+mkdir $HOME/.log
