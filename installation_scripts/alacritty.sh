548883ed50c1d33bb04d1a635271cf1886ad4eb9
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-03-16 05:30:23 +0200

    Improve alacritty installation script

--- installation_scripts/alacritty.sh
+++ installation_scripts/alacritty.sh
@@ -16,12 +16,12 @@ sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
 alacritty_source_tmp_path=/tmp/alacritty-$(date +%s)
 
 # clone alacritty from github
-git clone https://github.com/jwilm/alacritty.git $alacritty_source_tmp_path
+git clone https://github.com/jwilm/alacritty $alacritty_source_tmp_path
 
 # build alacritty from source
 cd $alacritty_source_tmp_path && cargo build --release
 
-sudo cp $alacritty_source_tmp_path/target/release/alacritty /usr/local/bin/alacritty
+yes | sudo cp -rf $alacritty_source_tmp_path/target/release/alacritty /usr/local/bin/alacritty
 cp alacritty.desktop ~/.local/share/applications
 
 rm -rf $alacritty_source_tmp_path
