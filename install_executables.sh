7784d1a93fd405e371b3d8210a2a03ba83b98f49
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-03-16 05:29:57 +0200

    Add wifi-stat bin and tcptrack installation script

--- install_executables.sh
+++ install_executables.sh
@@ -3,10 +3,4 @@
 # Create dir for executables
 mkdir -p $HOME/.local/bin
 
-BINARIES_DIR=$HOME/dotfiles/bin/
-
-executables_to_symlink="toggle-display-focus xcopy man"
-
-for executable in $executables_to_symlink; do
-  ln -nfs $BINARIES_DIR/$executable $HOME/.local/bin/$executable
-done
+ln -nfs $HOME/dotfiles/bin/* $HOME/.local/bin/
