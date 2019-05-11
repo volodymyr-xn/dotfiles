cefa394f9dc5d1a6624782179dd90be25c2c3787
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-05-11 17:53:52 +0300

    Add homebrew installation script

--- installation_scripts/fzf.sh
+++ installation_scripts/fzf.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# brew install ack
+
+sudo apt-get install silversearcher-ag
+
+# brew install the_silver_searcher
+
+# sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
+
 # Clone fzf repo from gitgub
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
