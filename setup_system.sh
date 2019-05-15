5c6564a24c5f3732cfd54e2b1aa90a3c0aadfe0a
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-05-15 14:28:53 +0300

    Move setup files under setup directory

--- setup_system.sh
+++ setup_system.sh
@@ -3,11 +3,11 @@
 ./installation_scripts/develop_libs.sh
 ./installation_scripts/linuxbrew.sh
 ./installation_scripts/asdf.sh
-./install_dotfiles.sh
 
-./install_software.sh
-./install_executables.sh
-./set_dconf.sh
+./setup/dotfiles.sh
+./setup/software.sh
+./setup/executables.sh
+./setup/dconf.sh
 
 # Create dir for home log
 mkdir -p $HOME/.log
