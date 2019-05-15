d85b9a604ca7627e811c68d1edd7c7dff988edb1
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-05-15 14:28:53 +0300

    Move setup files under setup directory

--- installation_scripts/rclone.sh
+++ installation_scripts/rclone.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# git clone https://github.com/ncw/rclone.git
+# cd rclone
+# go build
+
 go get -u -v github.com/ncw/rclone
 
 mv ${GOPATH}/bin/rclone $HOME/.local/bin/rclone
