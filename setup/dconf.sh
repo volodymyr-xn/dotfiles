cd23b82965852f124cbae68672e962bc48a65064
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-05-18 00:28:12 +0300

    Improve dconf setup

--- setup/dconf.sh
+++ setup/dconf.sh
@@ -15,7 +15,7 @@ gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'
 
 # Disable Caps Lock
 # dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:none']"
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none']"
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none', 'grp:alt_shift_toggle']"
 
 # Gnome terminal settings
 gnome_terminal=$(gsettings get org.gnome.Terminal.ProfilesList default)
@@ -26,13 +26,18 @@ gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/prof
 
 # Keybindings
 # Toggle display focus
-custom_keybinding_0_schema=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0
+keybindings_schema=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings
+
+custom_keybinding_0_schema=$keybindings_schema/custom0
 dconf write $custom_keybinding_0_schema/name "'Toggle display focus'"
 dconf write $custom_keybinding_0_schema/command "'toggle-display-focus'"
 dconf write $custom_keybinding_0_schema/binding "'<Control>space'"
 
 # Rofi setup
-custom_keybinding_1_schema=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1
+custom_keybinding_1_schema=$keybindings_schema/custom1
 dconf write $custom_keybinding_1_schema/name "'Rofi'"
 dconf write $custom_keybinding_1_schema/command "'rofi -combi-modi \"window,drun\" -show combi'"
 dconf write $custom_keybinding_1_schema/binding "'<Primary><Shift>f'"
+
+# Should be carefull here. It could broke the gnome
+dconf write $keybindings_schema "['$custom_keybinding_0_schema/', '$custom_keybinding_1_schema/']"
