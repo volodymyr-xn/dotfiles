47cfdf81ed497a0e7ba605a880ad70009e6613ef
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-05-17 23:34:39 +0300

    Improve dconf setup

--- setup/dconf.sh
+++ setup/dconf.sh
@@ -11,11 +11,18 @@ gsettings set org.gnome.desktop.interface icon-theme 'Numix-Circle'
 gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'
 
 # Set gnome shell theme
-gsettings set org.gnome.shell.extensions user-theme 'Arc-Dark'
+# gsettings set org.gnome.shell.extensions user-theme 'Arc-Dark'
 
 # Disable Caps Lock
 # dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:none']"
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none']"
+
+# Gnome terminal settings
+gnome_terminal=$(gsettings get org.gnome.Terminal.ProfilesList default)
+gnome_terminal=${gnome_terminal:1:-1} # remove leading and trailing single quotes
+gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$gnome_terminal/" font "Fira Mono Medium 14"
+
+# gconftool-2 set /apps/metacity/general/focus_new_windows --type string strict
 
 # Keybindings
 # Toggle display focus
@@ -24,12 +31,8 @@ dconf write $custom_keybinding_0_schema/name "'Toggle display focus'"
 dconf write $custom_keybinding_0_schema/command "'toggle-display-focus'"
 dconf write $custom_keybinding_0_schema/binding "'<Control>space'"
 
-# Gnome terminal settings
-gnome_terminal=$(gsettings get org.gnome.Terminal.ProfilesList default)
-gnome_terminal=${gnome_terminal:1:-1} # remove leading and trailing single quotes
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$gnome_terminal/" font "Fira Mono Medium 14"
-
-gconftool-2 set /apps/metacity/general/focus_new_windows --type string strict
-
-# /org/gnome/desktop/input-sources/xkb-options
-#   ['caps:none', 'grp:alt_shift_toggle']
+# Rofi setup
+custom_keybinding_1_schema=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1
+dconf write $custom_keybinding_1_schema/name "'Rofi'"
+dconf write $custom_keybinding_1_schema/command "'rofi -combi-modi \"window,drun\" -show combi'"
+dconf write $custom_keybinding_1_schema/binding "'<Primary><Shift>f'"
