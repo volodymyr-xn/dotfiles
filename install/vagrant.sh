c5888fdc95777074df72b028c8a43691b55a38c9
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-09-26 22:41:16 +0300

    Rename "installation_scripts" directory to "install"

---
+++ install/vagrant.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+version=${1:-"2.2.5"}
+deb_tmp_path=/tmp/vagrant-$version.deb
+
+wget https://releases.hashicorp.com/vagrant/$version/vagrant_$version\_x86_64.deb -O $deb_tmp_path
+
+yes | sudo dpkg -i $deb_tmp_path
