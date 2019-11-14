678ff0ca58a4fc876ba26cf93b3863b96d409072
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-11-14 03:19:34 +0200

    Minor updates

--- install/arc-theme.sh
+++ install/arc-theme.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 yes | sudo apt-get install autoconf automake libgtk-3-dev \
+                           sassc optipng inkscape \
                            gnome-themes-standard gnome-shell-extensions
 
 timestamp=$(date +%s)
 arc_theme_source_dir=/tmp/arc-theme-$timestamp
 
-git clone https://github.com/horst3180/arc-theme --depth 1 $arc_theme_source_dir
+git clone https://github.com/arc-design/arc-theme --depth 1 $arc_theme_source_dir
 cd $arc_theme_source_dir
 
 
