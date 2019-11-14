dc112e65d885ad53e787c5637c1c53260dd86037
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-11-14 03:19:34 +0200

    Minor updates

--- install/vim.sh
+++ install/vim.sh
@@ -28,4 +28,3 @@ curl -fLo ~/dotfiles/vim/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "VIM installed succesfully"
-# brew install vim
