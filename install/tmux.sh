bdac7bae66320d73a26caf3279abd8ead790c8c6
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-11-20 13:19:34 +0200

    Minor update

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-# sudo apt-get install -y automake autoconf \
-#                         build-essential pkg-config \
-#                         libevent-dev libncurses5-dev
-#
-# timestamp=$(date +%s)
-# tmux_source_dir=/tmp/tmux-$timestamp
-#
-# git clone https://github.com/tmux/tmux.git $tmux_source_dir
-#
-# cd $tmux_source_dir
-#
-# # tmux_version=$1
-#
-# # Checkout to last stable version
-# # git checkout $tmux_version
-#
-# sh autogen.sh
-# ./configure --prefix=$HOME/.local/
-#
-# make -j $(nproc)
-#
-# sudo make install
-# cd -
-# rm -rf $tmux_source_dir
+sudo apt-get install -y automake autoconf bison \
+                        build-essential pkg-config \
+                        libevent-dev libncurses5-dev
 
-brew install tmux
+timestamp=$(date +%s)
+tmux_source_dir=/tmp/tmux-$timestamp
+
+git clone https://github.com/tmux/tmux.git $tmux_source_dir
+
+cd $tmux_source_dir
+
+tmux_version=2.9a
+
+# Checkout to last stable version
+git checkout $tmux_version
+
+sh autogen.sh
+./configure --prefix=$HOME/.local/
+
+make -j $(nproc)
+
+sudo make install
+cd -
+rm -rf $tmux_source_dir
+
+# brew install tmux
