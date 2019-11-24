fbdcefd2055f6964759d24486213c6f1653bc88f
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-11-24 22:48:38 +0200

    Minor updates

--- install/rofi.sh
+++ install/rofi.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-version=1.5.1
-timestamp=$(date +%s)
-destination=/tmp/rofi-sources-$timestamp
-
-git clone --recursive https://github.com/DaveDavenport/rofi $destination
-
-cd $destination
-
-autoreconf -i
-
-mkdir build && cd build
-../configure
-
-make -j (nproc)
-sudo make install
+# version=1.5.1
+# timestamp=$(date +%s)
+# destination=/tmp/rofi-sources-$timestamp
+#
+# git clone --recursive https://github.com/DaveDavenport/rofi $destination
+#
+# cd $destination
+#
+# autoreconf -i
+#
+# mkdir build && cd build
+# ../configure PREFIX=$HOME/.local/
+#
+# make -j $(nproc)
+# make install
 
 # TO BE CONTINUED...
 # ON NEWER UBUNTU VERSION
+
+sudo apt install rofi -y
