b36078c1fbdce5aef0dfc4dfc16f0a4fcbd87487
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-12-05 00:14:39 +0200

    Add additional wine dependencies

--- install/wine.sh
+++ install/wine.sh
@@ -15,3 +15,8 @@ yes | wget -q https://dl.winehq.org/wine-builds/winehq.key -O- | sudo apt-key ad
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main'
 
 sudo apt-get install --install-recommends winehq-stable
+
+# Additional dependencies
+sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 \
+  libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 \
+  libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
