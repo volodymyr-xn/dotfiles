0717ecad69f8abc82135395e1cefd51107fd9acc
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-12-12 00:51:22 +0200

    Minor updates

--- install/icedtea.sh
+++ install/icedtea.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# sudo add-apt-repository ppa:maarten-fonville/ppa
-# sudo apt-get update
-# sudo apt-get install icedtea-8-plugin
+sudo add-apt-repository ppa:maarten-fonville/ppa
+sudo apt-get update
+sudo apt-get install icedtea-8-plugin
 
-mkdir -p ~/.mozilla/plugins
-rm ~/.mozilla/plugins/libnpjp2.so
-ln -s /usr/lib/jvm/java-9-oracle/lib/libnpjp2.so ~/.mozilla/plugins/
+# mkdir -p ~/.mozilla/plugins
+# rm ~/.mozilla/plugins/libnpjp2.so
+# ln -s /usr/lib/jvm/java-9-oracle/lib/libnpjp2.so ~/.mozilla/plugins/
--- install/java.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-yes | sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-yes | sudo apt-get install oracle-java9-installer
