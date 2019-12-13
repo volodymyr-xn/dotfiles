5efdf2ec5f1d1416f6de5993b7aba3b44cf9bb70
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-12-13 22:35:58 +0200

    Minor update

--- install/postgres.sh
+++ install/postgres.sh
@@ -2,9 +2,7 @@
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 # sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg_bionic.list'
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > /etc/apt/sources.list.d/pgdg_xenial.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lbs_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg_xenial.list'
 
 sudo apt update
-sudo apt-get install postgresql-11
-
-
+sudo apt-get install postgresql-12
