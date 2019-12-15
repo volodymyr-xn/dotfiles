df6ac0e78f23e01dacb8d0e2339187e48b2e2744
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-12-15 16:29:36 +0200

    Minor update

--- install/photoshop_wine_dependencies.sh
+++ install/photoshop_wine_dependencies.sh
@@ -3,4 +3,5 @@
 winetricks \
   fontsmooth=rgb gdiplus vcrun2008 vcrun2010 \
   vcrun2012 vcrun2013 vcrun2015 atmlib \
-  msxml3 msxml6 gdiplus corefonts mfc42
+  fontsmooth-rgb gecko \
+  msxml3 msxml4 msxml6 gdiplus corefonts mfc40 mfc42
--- install/wine.sh
+++ install/wine.sh
@@ -20,3 +20,6 @@ sudo apt-get install --install-recommends winehq-stable
 sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 \
   libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 \
   libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
+
+# for supreme commander
+winetricks dlls xact
