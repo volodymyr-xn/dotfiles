9bba791c42b5193bb5a264f16f67c46c3cc653e8
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-12-22 14:43:42 +0200

    Minor update

--- install/photoshop_wine_dependencies.sh
+++ install/photoshop_wine_dependencies.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-winetricks \
-  fontsmooth=rgb gdiplus vcrun2008 vcrun2010 \
-  vcrun2012 vcrun2013 vcrun2015 atmlib \
-  fontsmooth-rgb gecko \
-  msxml3 msxml4 msxml6 gdiplus corefonts mfc40 mfc42
+winetricks fontsmooth=rgb gdiplus vcrun2008 vcrun2010  vcrun2012 vcrun2013 vcrun2015 atmlib msxml4 msxml6 gdiplus corefonts mfc40 mfc42
