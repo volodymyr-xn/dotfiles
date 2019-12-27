e314b85c28efc635ae4dfa80875985b3c19bec22
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2019-12-27 20:47:26 +0200

    Minor update

--- install/tor.sh
+++ install/tor.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-yes | sudo apt-get install tor
-yes | sudo apt-get install privoxy
+yes | sudo apt-get install tor privoxy
 
 sudo systemctl stop tor
 sudo systemctl disable tor
+sudo systemctl disable tor@default.service
 
 sudo systemctl stop privoxy
 sudo systemctl disable privoxy
-sudo systemctl disable tor@default.service
