1bd3927ec808fa57fa28ac03de18021a71feaeb9
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2020-01-08 19:32:27 +0200

    Minor update

--- install/tor.sh
+++ install/tor.sh
@@ -8,3 +8,10 @@ sudo systemctl disable tor@default.service
 
 sudo systemctl stop privoxy
 sudo systemctl disable privoxy
+
+# to enable Privoxy connection to Tor
+
+# Add this to `/etc/privoxy/config`:
+# `forward-socks4a / localhost:9050 .`
+
+# `9050` is tor default port
