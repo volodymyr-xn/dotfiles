6df9def6fbb94816a4cd0c82dcde7c0a4cd96a3b
Volodymyr Shevchuk <volodymyr.shevchuk93@gmail.com>
2020-05-02 13:08:52 +0300

    Minor update

--- install/fzf.sh
+++ install/fzf.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# Search engine for FZF
+brew install the_silver_searcher
+
+# Alternative search engine
 # brew install ack
+
+# Build dependencies for FZF
 # sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
 
 # Clone fzf repo from gitgub
--- install/tlp.sh
+++ install/tlp.sh
@@ -5,4 +5,15 @@
 
 sudo add-apt-repository ppa:linrunner/tlp
 
-yes | sudo apt install tlp tlp-rdw x86_energy_perf_policy
+yes | sudo apt install tlp tlp-rdw
+
+# ThinkPad only
+# sudo apt install acpi-call-dkms tp-smapi-dkms
+
+# External kernel module providing battery charge thresholds
+# and recalibration for newer ThinkPads (X220/T420 and later)
+# acpi-call-dkms (universe) – optional
+
+# External kernel module providing battery charge thresholds,
+# recalibration and specific tlp-stat -b output for older ThinkPads
+# tp-smapi-dkms (PPA or universe) – optional
