e17b92e180f999b30631018eb2169af8146d7bfd
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-08-30 00:20:35 +0300

    Update

--- machine_setup/mac.sh
+++ machine_setup/mac.sh
@@ -3,21 +3,7 @@
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install \
-  vim \
-  nvim \
-  tmux \
-  cmake \
-  fzf \
-  rg \
-  mise \
-  the_silver_searcher \
-  jemalloc \
-  fd \
-  imagemagic \
-  wget \
-  vips
+brew bundle --file $HOME/dotfiles/machine_setup/Brewfile
 
-brew install --cask ghostty
 
 $HOME/dotfiles/install/oh-my-zsh.sh
