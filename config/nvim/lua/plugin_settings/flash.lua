970c45f1ba12e82c249b77c87ff487a709994126
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-10-10 02:32:45 +0300

    Update nvim

--- config/nvim/lua/plugin_settings/flash.lua
+++ config/nvim/lua/plugin_settings/flash.lua
@@ -151,8 +151,25 @@ require("flash").setup({
     -- `f`, `F`, `t`, `T`, `;` and `,` motions
     char = {
       enabled = true,
+      -- dynamic configuration for ftFT motions
+      config = function(opts)
+        -- autohide flash when in operator-pending mode
+        opts.autohide = opts.autohide or (vim.fn.mode(true):find("no") and vim.v.operator == "y")
+
+        -- disable jump labels when not enabled, when using a count,
+        -- or when recording/executing registers
+        opts.jump_labels = opts.jump_labels
+        and vim.v.count == 0
+        and vim.fn.reg_executing() == ""
+        and vim.fn.reg_recording() == ""
+
+        -- Show jump labels only in operator-pending mode
+        -- opts.jump_labels = vim.v.count == 0 and vim.fn.mode(true):find("o")
+      end,
+      -- hide after jump when not using jump labels
+      autohide = false,
       -- show jump labels
-      jump_labels = false,
+      jump_labels = true,
       -- set to `false` to use the current line only
       multi_line = false,
       -- When using jump labels, don't use these keys
@@ -162,22 +179,10 @@ require("flash").setup({
       -- by removing them from the list.
       -- If you rather use another key, you can map them
       -- to something else, e.g., { [";"] = "L", [","] = H }
-      keys = {'f'},
+      keys = {"f"},
       ---@alias Flash.CharActions table<string, "next" | "prev" | "right" | "left">
       -- The direction for `prev` and `next` is determined by the motion.
       -- `left` and `right` are always left and right.
-      char_actions = function(motion)
-        return {
-          [";"] = "next", -- set to `right` to always go right
-          [","] = "prev", -- set to `left` to always go left
-          -- clever-f style
-          [motion:lower()] = "next",
-          [motion:upper()] = "prev",
-          -- jump2d style: same case goes next, opposite case goes prev
-          -- [motion] = "next",
-          -- [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
-        }
-      end,
       search = { wrap = false },
       highlight = { backdrop = true },
       jump = {
