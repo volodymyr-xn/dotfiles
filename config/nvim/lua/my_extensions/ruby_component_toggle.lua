c8659305e85a826c1aebf0a340a3e0381de9d53b
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-02-06 13:33:06 +0200

    Update

--- config/nvim/lua/my_extensions/ruby_component_toggle.lua
+++ config/nvim/lua/my_extensions/ruby_component_toggle.lua
@@ -15,11 +15,12 @@ function M.navigate_to_extension(target_extension)
   
   local base_name = get_base_name(current_filename)
   local target_file = current_dir .. '/' .. base_name .. target_extension
+  local target_filename = base_name .. target_extension
   
   if vim.fn.filereadable(target_file) == 1 then
     vim.cmd('edit ' .. vim.fn.fnameescape(target_file))
   else
-    vim.notify('File not found: ' .. target_file, vim.log.levels.ERROR)
+    vim.notify('File not found: ' .. target_filename, vim.log.levels.ERROR)
   end
 end
 
@@ -36,7 +37,112 @@ function M.navigate_to_style()
   elseif vim.fn.filereadable(css_file) == 1 then
     vim.cmd('edit ' .. vim.fn.fnameescape(css_file))
   else
-    vim.notify('Style file not found: ' .. base_name .. '.scss or .css', vim.log.levels.ERROR)
+    vim.notify('File not found: ' .. base_name .. '.scss or ' .. base_name .. '.css', vim.log.levels.ERROR)
+  end
+end
+
+function M.toggle_alternate()
+  local current_dir = vim.fn.expand('%:p:h')
+  local current_filename = vim.fn.expand('%:t')
+  
+  local target_file = nil
+  local target_filename = nil
+  
+  if current_filename:match('%.rb$') then
+    local base_name = current_filename:gsub('%.rb$', '')
+    target_file = current_dir .. '/' .. base_name .. '.html.erb'
+    target_filename = base_name .. '.html.erb'
+  elseif current_filename:match('%.html%.erb$') then
+    local base_name = current_filename:gsub('%.html%.erb$', '')
+    target_file = current_dir .. '/' .. base_name .. '.rb'
+    target_filename = base_name .. '.rb'
+  else
+    vim.notify('Not a Ruby component file (.rb or .html.erb)', vim.log.levels.WARN)
+    return
+  end
+  
+  if vim.fn.filereadable(target_file) == 1 then
+    vim.cmd('edit ' .. vim.fn.fnameescape(target_file))
+  else
+    vim.notify('File not found: ' .. target_filename, vim.log.levels.ERROR)
+  end
+end
+
+function M.toggle_js_erb()
+  local current_dir = vim.fn.expand('%:p:h')
+  local current_filename = vim.fn.expand('%:t')
+  
+  local target_file = nil
+  local target_filename = nil
+  
+  if current_filename:match('%.js$') then
+    local base_name = current_filename:gsub('%.js$', '')
+    target_file = current_dir .. '/' .. base_name .. '.html.erb'
+    target_filename = base_name .. '.html.erb'
+  elseif current_filename:match('%.html%.erb$') then
+    local base_name = current_filename:gsub('%.html%.erb$', '')
+    target_file = current_dir .. '/' .. base_name .. '.js'
+    target_filename = base_name .. '.js'
+  else
+    local base_name = get_base_name(current_filename)
+    target_file = current_dir .. '/' .. base_name .. '.js'
+    target_filename = base_name .. '.js'
+  end
+  
+  if vim.fn.filereadable(target_file) == 1 then
+    vim.cmd('edit ' .. vim.fn.fnameescape(target_file))
+  else
+    vim.notify('File not found: ' .. target_filename, vim.log.levels.ERROR)
+  end
+end
+
+function M.toggle_erb_style()
+  local current_dir = vim.fn.expand('%:p:h')
+  local current_filename = vim.fn.expand('%:t')
+  
+  local target_file = nil
+  local target_filename = nil
+  
+  if current_filename:match('%.html%.erb$') then
+    local base_name = current_filename:gsub('%.html%.erb$', '')
+    local scss_file = current_dir .. '/' .. base_name .. '.scss'
+    local css_file = current_dir .. '/' .. base_name .. '.css'
+    
+    if vim.fn.filereadable(scss_file) == 1 then
+      vim.cmd('edit ' .. vim.fn.fnameescape(scss_file))
+      return
+    elseif vim.fn.filereadable(css_file) == 1 then
+      vim.cmd('edit ' .. vim.fn.fnameescape(css_file))
+      return
+    else
+      vim.notify('File not found: ' .. base_name .. '.scss or ' .. base_name .. '.css', vim.log.levels.ERROR)
+      return
+    end
+  elseif current_filename:match('%.scss$') or current_filename:match('%.css$') then
+    local base_name = get_base_name(current_filename)
+    target_file = current_dir .. '/' .. base_name .. '.html.erb'
+    target_filename = base_name .. '.html.erb'
+  else
+    local base_name = get_base_name(current_filename)
+    local scss_file = current_dir .. '/' .. base_name .. '.scss'
+    local css_file = current_dir .. '/' .. base_name .. '.css'
+    
+    if vim.fn.filereadable(scss_file) == 1 then
+      vim.cmd('edit ' .. vim.fn.fnameescape(scss_file))
+      return
+    elseif vim.fn.filereadable(css_file) == 1 then
+      vim.cmd('edit ' .. vim.fn.fnameescape(css_file))
+      return
+    else
+      vim.notify('File not found: ' .. base_name .. '.scss or ' .. base_name .. '.css', vim.log.levels.ERROR)
+      return
+    end
+  end
+  
+  if vim.fn.filereadable(target_file) == 1 then
+    vim.cmd('edit ' .. vim.fn.fnameescape(target_file))
+  else
+    vim.notify('File not found: ' .. target_filename, vim.log.levels.ERROR)
   end
 end
 
