9cc927215e3657afbcafb014e889145eca52bfbe
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-04-04 16:11:09 +0300

    Update markview config

--- nvim/lua/plugin_settings/markview.lua
+++ nvim/lua/plugin_settings/markview.lua
@@ -11,7 +11,7 @@ require("markview").setup({
     list_items = {
       indent_size = 2,
       shift_width = 2,
-      marker_minus = { add_padding = false, text = "●", hl = "MarkviewListItemMinus" },
+      marker_minus = { add_padding = true, text = "●", hl = "MarkviewListItemMinus" },
       marker_plus = { add_padding = false, text = "◈", hl = "MarkviewListItemPlus" },
       marker_star = { add_padding = false, text = "◇", hl = "MarkviewListItemStar" },
     },
